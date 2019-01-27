f9962071df22c5700dc62b7a222125792a80f045
Fabio Abreu <fnmabreu@gmail.com>
2019-01-27 10:20:48 +0000

    Build a Database Web App - Spring MVC and Hibernate Project - Part 3

---
+++ web-customer-tracker/src/com/omega/springdemo/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.omega.springdemo.service;
+
+import java.util.List;
+
+import com.omega.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+}
