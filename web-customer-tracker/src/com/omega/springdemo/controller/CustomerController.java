7df55851f8640d4896f7c43dd4d499de13b08f3d
Fabio Abreu <fnmabreu@gmail.com>
2019-01-27 00:22:30 +0000

    Build a Database Web App - Spring MVC and Hibernate Project - Part 2

--- web-customer-tracker/src/com/omega/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/omega/springdemo/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.omega.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.omega.springdemo.dao.CustomerDAO;
+import com.omega.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+
+		// get customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
 
 		return "list-customers";
 	}
---
+++ web-customer-tracker/src/com/omega/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.omega.springdemo.dao;
+
+import java.util.List;
+
+import com.omega.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
