db7dfa6374e018f3270ecd97df83f53888feddfc
Fabio Abreu <fnmabreu@gmail.com>
2019-01-28 19:55:04 +0000

    Aspect-Oriented Programming: @Before Advice Type

---
+++ spring-demo-aop/src/com/omega/aopdemo/MainDemoApp.java
@@ -0,0 +1,30 @@
+package com.omega.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.omega.aopdemo.dao.AccountDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// call the business method
+		theAccountDAO.addAccount();
+		
+		System.out.println("\n let's call it again!\n");
+		
+		// call the business method again
+		theAccountDAO.addAccount();
+		
+		// close the context
+		context.close();
+
+	}
+
+}
