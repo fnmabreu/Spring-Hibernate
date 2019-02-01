a339c2214886aa046402ba82fe4bc0ac6add1bb6
Fabio Abreu <fnmabreu@gmail.com>
2019-02-01 19:57:30 +0000

    Modified/Renamed projects pointcut-declarations-combo & order-aspects

--- spring-demo-aop-pointcut-order-aspects/src/com/omega/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-order-aspects/src/com/omega/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,43 +2,17 @@ package com.omega.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-/**
- * Match methods in a DAO package and exclude getter/setter methods
- * 
- */
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 
-	@Pointcut("execution(* com.omega.aopdemo.dao.*.*(..))")
-	private void forDaoPackage() {
-	}
-
-	// create a pointcut for getter methods
-	@Pointcut("execution(* com.omega.aopdemo.dao.*.get*(..))")
-	private void getter() {
-	}
-
-	// create a pointcut for setter methods
-	@Pointcut("execution(* com.omega.aopdemo.dao.*.set*(..))")
-	private void setter() {
-	}
-
-	// create pointcut: include package ... exclude getter/setter
-	@Pointcut("forDaoPackage() && !(getter() || setter())")
-	private void forDaoPackageNoGetterSetter() {
-	}
-
-	@Before("forDaoPackageNoGetterSetter()")
+	@Before("com.omega.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>>> Executing @Before advice on method");
 	}
 
-	@Before("forDaoPackageNoGetterSetter()")
-	public void performApiAnalytcs() {
-		System.out.println("\n=====>>> Performing API analytics");
-	}
 }
