afb21b5bb4a5646130ee22717804cbb55cb10292
Fabio Abreu <fnmabreu@gmail.com>
2019-02-05 00:15:27 +0000

    Spring Security - Adding Custom Login Form

--- spring-security-demo/src/main/java/com/omega/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/omega/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.omega.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -18,11 +19,22 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 		@SuppressWarnings("deprecation")
 		UserBuilder users = User.withDefaultPasswordEncoder();
 
-		auth.inMemoryAuthentication()
-				.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+		auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"))
 				.withUser(users.username("mary").password("test123").roles("MANAGER"))
 				.withUser(users.username("susan").password("test123").roles("ADMIN"));
 
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/showLoginPage")
+				.loginProcessingUrl("/authenticateUser")
+				.permitAll();
 	}
 
 }
---
+++ spring-security-demo/src/main/java/com/omega/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.omega.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("showLoginPage")
+	public String showLoginPage() {
+
+		return "plain-login";
+	}
+}
