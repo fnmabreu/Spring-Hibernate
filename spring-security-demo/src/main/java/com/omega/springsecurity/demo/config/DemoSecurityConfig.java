18ec6facfedfbf3dcf8dcbbec4f2ffb43c63d1fc
fnmabreu <fnmabreu@gmail.com>
2019-02-10 01:13:12 +0000

    Spring Security - Add JDBC Database Authentication
    DataSource Configuration

--- spring-security-demo/src/main/java/com/omega/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/omega/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,28 +1,26 @@
 package com.omega.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	// add reference to our security datasource
+	@Autowired
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		// add users for in memory authentication
-		@SuppressWarnings("deprecation")
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-				.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-				.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
-
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 	}
 
 	@Override
